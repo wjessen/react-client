6275f7cda70dfc63f063653ac1459a5a34227c68
Wayne Jessen <wjessen@thefarmersdog.com>
2023-02-18 21:53:58 -0800

    making the double click editable

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
 function App() {
-
-  fetch('https://clownfish-app-4kruw.ondigitalocean.app/')
-    .then(response => response.json())
-    .then(data => console.log(data));
-  console.log("Here")
+  // const [count, setCount] = useState(0);
+  // const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks' }, { text: 'Learn React' }]);
+  // fetch('https://clownfish-app-4kruw.ondigitalocean.app/')
+  //   .then(response => response.json())
+  //   .then((data) => { );
+  // console.log("Here")
+  function textHandler(text: string, index: number) {
+    todos[index].text = text;
+    setTodos([...todos]);
+  }
   return (
     <div className="App">
-      <p>Here23</p>
+      <table>
+        {todos.map((todo, index) => (
+          <div>
+          <tr key={index}>
+            <TodoItem text={todo.text} index={index} textHandler={textHandler}></TodoItem>
+          </tr>
+          <tr><td>{todo.text}</td></tr>
+          </div>
+        ))}
+      </table>
     </div>
   );
 }
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+// import './App.css';
+
+
+function TodoItem(props: any) {
+
+    const [isEditable, setIsEditable] = useState(props.isEditable);
+    const [text, setText] = useState(props.text);
+
+    const handleDoubleClick = (event: any) => {
+        if(event.detail !== 2){
+          return;
+        }
+        setIsEditable(!isEditable);
+        console.log("Double Clicked");
+    }
+
+    const handleSingleClick = (event: any) => {
+        if(event.detail !== 1){
+            return;
+        }
+        console.log("Single Clicked")
+    }
+
+    const handleClick = (event: any) => {
+        handleSingleClick(event);
+        handleDoubleClick(event);
+
+    }
+    const handleChange = (event: any) => {
+        setText(event.target.value);
+        props.textHandler(event.target.value, props.index);
+    }
+    if (isEditable) {
+        return(
+            <div>
+            <input onClick={handleClick}  onChange={handleChange} type="text" value={text}>
+            </input>
+
+            </div>
+        )
+    }
+    return(
+        <div>
+            <p onClick={handleClick} onChange={handleChange}>{text}</p>
+        </div>
+    )
+
+}
+
+export default TodoItem;
